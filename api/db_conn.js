0e7dea2cfcd26ba7930d9a53fd9684bf1b89fe0f
megastar98 <obikoya11@gmail.com>
2018-09-13 10:28:42 +0100

    online db

--- api/db_conn.js
+++ api/db_conn.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/inventory-system').then(()=>{
+// mongoose.connect('mongodb://localhost/inventory-system').then(()=>{
+//   console.log('successfully connected to the database')
+// }).catch((err)=>{
+//   console.log('An error occured: '+err);
+// })
+
+mongoose.connect(`mongodb://nacoss:nacoss1>@ds253922.mlab.com:53922/inventory-system`).then(()=>{
   console.log('successfully connected to the database')
 }).catch((err)=>{
   console.log('An error occured: '+err);
